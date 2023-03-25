5a62c5e0a643e33a44793dc0a035a1780f684b33
Qrcis <103064730+Qrcis@users.noreply.github.com>
2023-03-25 13:01:07 +0330

    Update Config.py

--- Config.py
+++ Config.py
@@ -17,11 +17,11 @@ if ENVIRONMENT:
         MUST_JOIN = MUST_JOIN.replace("@", "")
 else:
     # Fill the Values
-    API_ID = 0
-    API_HASH = ""
-    BOT_TOKEN = ""
+    API_ID = 13823061
+    API_HASH = "d9a371842b5dd4aa9bb557fd23ac44f4"
+    BOT_TOKEN = "5809206032:AAGjvK-BidiI-cmOiLP7lbVRIVrx1Ul8byw"
     DATABASE_URL = ""
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
-    MUST_JOIN = ""
+    MUST_JOIN = "OPStxt"
     if MUST_JOIN.startswith("@"):
         MUST_JOIN = MUST_JOIN[1:]
